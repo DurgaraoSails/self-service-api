924e5cbb4c58a2bff75c1b68c365bebd57b16cdf
Ram Supreeth <mramsupreeth@gmail.com>
2026-09-10 12:56:54 +0530

    moved microsoft related files to application.yaml file (#51)
    * moved microsoft related files to application.yaml file
    
    * Update application.yaml

--- src/main/java/com/sails/ai/selfserviceapi/auth/microsoft/MicrosoftSsoConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.sails.ai.selfserviceapi.auth.microsoft;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:microsoft-sso.properties")
-public class MicrosoftSsoConfiguration {}
